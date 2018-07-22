8aae4afc0c204211fe3372dd0e0fa49bed472e8b
Timon <3tbrueni@informatik.uni-hamburg.de>
2018-07-22 20:18:14 +0200

    adjusted init data

--- src/app/components/heart/data.ts
+++ src/app/components/heart/data.ts
@@ -7,47 +7,11 @@ export var single = [
   
   export var multi = [
     {
-      "name": "15:12 Uhr",
+      "name": "11:46 Uhr",
       "series": [
         {
           "name": "0",
-          "value": 72
-        },
-        {
-          "name": "1",
-          "value": 65
-        },
-        {
-          "name": "2",
-          "value": 55
-        },
-        {
-          "name": "3",
-          "value": 58
-        },
-        {
-          "name": "4",
-          "value": 49
-        },
-        {
-          "name": "5",
-          "value": 56
-        },
-        {
-          "name": "6",
-          "value": 62
-        },
-        {
-          "name": "7",
-          "value": 53
-        },
-        {
-          "name": "8",
-          "value": 52
-        },
-        {
-          "name": "9",
-          "value": 56
+          "value": 0
         },
       ]
     }
